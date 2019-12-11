8388ab6a7167623ef81742f67df9052c7bb7c2d6
mac <mauricio.bleck7@gmail.com>
2019-12-10 23:45:33 -0300

    estruturado as rotas, os models e o primeiro controller

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::namespace('Station')->group(function(){
+
+    Route::prefix('station')->group(function(){
+        //sensors
+        Route::resource('/sensors', 'AdressesController');
+
+        //stations
+        Route::resource('/stations', 'AdressesController');
+
+        //data
+        Route::resource('/data', 'AdressesController');
+    });
+});
